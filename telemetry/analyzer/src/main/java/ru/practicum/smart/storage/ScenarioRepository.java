4916202f2eef3051275cda893b5c4f6549cb31af
VolkovA <va771@yandex.ru>
2026-01-25 18:35:07 +0300

    Добавил запрос в репозиторий

--- telemetry/analyzer/src/main/java/ru/practicum/smart/storage/ScenarioRepository.java
+++ telemetry/analyzer/src/main/java/ru/practicum/smart/storage/ScenarioRepository.java
@@ -1,13 +1,19 @@
 package ru.practicum.smart.storage;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import ru.practicum.smart.model.Scenario;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ScenarioRepository extends JpaRepository<Scenario, Long> {
-    List<Scenario> findByHubId(String hubId);
+    @Query("SELECT s FROM Scenario s " +
+            "LEFT JOIN FETCH s.conditions " +
+            "LEFT JOIN FETCH s.actions " +
+            "WHERE s.hubId = :hubId")
+    List<Scenario> findByHubId(@Param("hubId") String hubId);
 
     Optional<Scenario> findByHubIdAndName(String hubId, String name);
 }
