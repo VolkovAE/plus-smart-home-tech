6146c8fda39269cfb24fd28716609cade789eceb
VolkovA <va771@yandex.ru>
2026-01-25 19:13:23 +0300

    Логирование - 4 убрал автогенерацию id сенсора и запретил null

--- telemetry/analyzer/src/main/java/ru/practicum/smart/model/Sensor.java
+++ telemetry/analyzer/src/main/java/ru/practicum/smart/model/Sensor.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 import lombok.experimental.FieldDefaults;
 
 import static ru.practicum.smart.util.StringConstants.TABLE_NAME_ENTITY_SENSOR;
@@ -12,12 +13,14 @@ import static ru.practicum.smart.util.StringConstants.TABLE_NAME_ENTITY_SENSOR;
 @Table(name = TABLE_NAME_ENTITY_SENSOR)
 @Setter
 @Getter
+@ToString
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class Sensor {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
     String id;
 
-    @Column(name = "hub_id")
+    @Column(name = "hub_id", nullable = false)
     private String hubId;
 }
