5ccf53a1af68e586f522e0bada0d3040c013dabd
VolkovA <va771@yandex.ru>
2026-02-02 19:15:39 +0300

    Сделал заготовку для использования Feign-клиента

--- commerce/shopping-cart/src/main/java/ru/practicum/smart/ShoppingCartApp.java
+++ commerce/shopping-cart/src/main/java/ru/practicum/smart/ShoppingCartApp.java
@@ -3,9 +3,11 @@ package ru.practicum.smart;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableFeignClients
 public class ShoppingCartApp {
     public static void main(String[] args) {
         SpringApplication.run(ShoppingCartApp.class, args);
--- commerce/shopping-store/src/main/java/ru/practicum/smart/ShoppingStoreApp.java
+++ commerce/shopping-store/src/main/java/ru/practicum/smart/ShoppingStoreApp.java
@@ -3,9 +3,11 @@ package ru.practicum.smart;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableFeignClients
 public class ShoppingStoreApp {
     public static void main(String[] args) {
         SpringApplication.run(ShoppingStoreApp.class, args);
