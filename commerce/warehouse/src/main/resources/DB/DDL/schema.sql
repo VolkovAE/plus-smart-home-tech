b5b011db1b4e3414c1dca632273a3989bee3e66f
VolkovA <va771@yandex.ru>
2026-02-05 20:27:37 +0300

    Подготовил шаблон warehouse

---
+++ commerce/warehouse/src/main/resources/DB/DDL/schema.sql
@@ -0,0 +1,14 @@
+;
+--одна команда должны быть, если не будет запросов'
+CREATE SCHEMA IF NOT EXISTS warehouse;
+
+CREATE TABLE IF NOT EXISTS warehouse.products
+(
+    product_Id VARCHAR(36) PRIMARY KEY,
+    depth NUMERIC(15,2),
+    height NUMERIC(15,2),
+    width NUMERIC(15,2),
+    quantity INTEGER NOT NULL DEFAULT 1,
+    weight NUMERIC(15,3),
+    fragile BOOLEAN DEFAULT FALSE
+);
