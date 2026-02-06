f6d196c6ec9c33f9cfeb7789cbbdb64aa361a916
VolkovA <va771@yandex.ru>
2026-02-06 23:02:11 +0300

    осталось еще 4 ошибки - продолжаем угадывать)

--- commerce/shopping-store/src/main/resources/DB/DDL/schema.sql
+++ commerce/shopping-store/src/main/resources/DB/DDL/schema.sql
@@ -6,8 +6,8 @@ CREATE TABLE IF NOT EXISTS shopping_store.products (
     product_Id VARCHAR(36) PRIMARY KEY,
     product_Name  VARCHAR(150) NOT NULL CHECK(TRIM('	 ' FROM description) != ''),
     description VARCHAR NOT NULL CHECK(TRIM('	 ' FROM description) != ''),
-    quantity_State VARCHAR(10) NOT NULL CHECK(TRIM('	 ' FROM description) != ''),
-    product_State VARCHAR(15) NOT NULL CHECK(TRIM('	 ' FROM description) != ''),
+    quantity_State VARCHAR(25) NOT NULL CHECK(TRIM('	 ' FROM description) != ''),
+    product_State VARCHAR(25) NOT NULL CHECK(TRIM('	 ' FROM description) != ''),
     product_Category VARCHAR(25) NOT NULL CHECK(TRIM('	 ' FROM description) != ''),
     price NUMERIC(15, 2) NOT NULL,
     image_Src VARCHAR
